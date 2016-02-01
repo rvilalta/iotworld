6ca9a9c1a027d1f882c90bcab51b92d8fe2487a6
Ricard <rvilalta@gmail.com>
2016-02-01 12:07:18 +0100

    Updating algo

--- algo.py
+++ algo.py
@@ -7,16 +7,22 @@ TIME_WINDOW = 1
 
 N_FLOWS = 5
 
-T_MAX = 3 * TIME_WINDOW
+T_MAX = 1000 * TIME_WINDOW
 
 flow_results_db = {}
 
-N_SIGMA = 1.2
+values_db = {}
+
+N_SIGMA =10
+WINDOW = 10
+#CHECK_PARAM='packets_per_second'
+CHECK_PARAM='bytes_per_second'
 
 ALGOERRORS = 0
 ALGODECISIONS = 0
 FLOWS = 0
 BADFLOWS = 0
+BADFLOWS_ERROR = 0
 
 def create_bad_flow (percent=10) :
     return random.randrange(100) < percent
@@ -25,19 +31,20 @@ def read_flows(T) :
     #print "reading flows"
     for i in range(0, N_FLOWS):
         #select if flow is ok or not
-        
+        base_pkts_sec = 20
         if not create_bad_flow():
             flow_results_db[i]['timestamp'].append( time.time() )
-            flow_results_db[i]['packets_per_second'].append( i + random.randint( i, i+2) )
+            flow_results_db[i]['packets_per_second'].append( base_pkts_sec + random.randint( 0, i+4) )
             flow_results_db[i]['bytes_per_second'].append( 100 * flow_results_db[i]['packets_per_second'][-1] )
             flow_results_db[i]['bad_flow'] = False
         else:
             #print "Creating dangerous flows"
             flow_results_db[i]['timestamp'].append( time.time() )
-            flow_results_db[i]['packets_per_second'].append( 1.5*(i + random.randint( i, i+2) )  )
+            flow_results_db[i]['packets_per_second'].append( 2*( base_pkts_sec + random.randint( 0, i+4) )  )
             flow_results_db[i]['bytes_per_second'].append( 150*flow_results_db[i]['packets_per_second'][-1] )
             flow_results_db[i]['bad_flow'] = True
         #print "Flow: " + str(i) + " packets_per_second " + str(flow_results_db[i]['packets_per_second'][-1]) + " bytes_per_second: " + str(flow_results_db[i]['bytes_per_second'][-1])
+        
 
 #def mean function 
 def mean(values):
@@ -54,44 +61,60 @@ def stanDev(values):
 	return math.sqrt(under_root)
 	
 def check_flow(flow_id):
-    global ALGODECISIONS, ALGOERRORS, FLOWS, BADFLOWS
+    global ALGODECISIONS, ALGOERRORS, FLOWS, BADFLOWS, BADFLOWS_ERROR
     ALGODECISIONS = ALGODECISIONS + 1
     FLOWS+=1
-    #check pkts_per_sec
-    check_flow_param(flow_id, 'packets_per_second' )
-
-    #check bytes_per_sec
-    result = check_flow_param(flow_id, 'bytes_per_second' )
-    if flow_results_db[flow_id]['bad_flow']:
-        BADFLOWS += 1
-    if (result != flow_results_db[flow_id]['bad_flow'] ):
-        print "ALGO OK"
-        
+    result = False
+    if CHECK_PARAM=='packets_per_second':
+        #check pkts_per_sec
+        result = check_flow_param(flow_id, 'packets_per_second' )
     else:
-        print "ALGO FAILED"
-        ALGOERRORS = ALGOERRORS + 1
+        #check bytes_per_sec
+        result = check_flow_param(flow_id, 'bytes_per_second' )
+
+    #evaluating algorithm
+    if ALGODECISIONS >= WINDOW * N_FLOWS :
+        if flow_results_db[flow_id]['bad_flow']:
+            BADFLOWS += 1
+            print "Checking a bad flow"
+        if (result != flow_results_db[flow_id]['bad_flow'] ):
+            print "ALGO OK"
+        else:
+            print "ALGO FAILED"
+            ALGOERRORS = ALGOERRORS + 1
+            
+            if flow_results_db[flow_id]['bad_flow']:
+                BADFLOWS_ERROR = BADFLOWS_ERROR +1
     
     
     print "ALGORITHM ERROR PERCENTAGE: " + str( ALGOERRORS * 100.0 / ALGODECISIONS ) + "%"
     if BADFLOWS > 1:
-        print "BAD FLOWS NOT DETECTED PERCENTAGE: " + str( ALGOERRORS * 100.0 / BADFLOWS ) + "%"
+        print "BAD FLOWS NOT DETECTED PERCENTAGE: " + str( BADFLOWS_ERROR * 100.0 / BADFLOWS ) + "%"
     print "BAD FLOWS PERCENTAGE: " + str( BADFLOWS * 100.0 / FLOWS ) + "%"
 
 def check_flow_param(flow_id, param) :
-    values = flow_results_db[flow_id][param][0:-1]
-    #print values
-    if len(values) < 2 :
+    values = values_db[flow_id]
+    #values = flow_results_db[flow_id][param][0:-1]
+    print values
+    if len(values) < WINDOW :
+        print "Not checking. SMALL WINDOW. Adding: " + str(flow_results_db[flow_id][param][-1])
+        values_db[flow_id].append(flow_results_db[flow_id][param][-1])
         return True
+
     #condicion
-    #print "Flow: " + str(flow_id) + " Current param " + param + " mean: " + str(mean(values)) + " stanDev: " + str(stanDev(values))
-    threshold = mean(values)+N_SIGMA*stanDev(values)
+    print "Flow: " + str(flow_id) + " Current param " + param + " mean: " + str(mean(values)) + " stanDev: " + str(stanDev(values))
+    threshold_up = mean(values)+N_SIGMA*stanDev(values)
+    threshold_down = mean(values)-N_SIGMA*stanDev(values)
 
-    #print "Flow: " + str(flow_id) + " Current param " + param + " value: " + str(flow_results_db[flow_id][param][-1]) + " threshold: " + str(threshold)
+    print "Flow: " + str(flow_id) + " Current param " + param + " value: " + str(flow_results_db[flow_id][param][-1]) + " threshold_up: " + str(threshold_up) + " threshold_down: " + str(threshold_down)
 
-    if flow_results_db[flow_id][param][-1] >  threshold:
-        #print "Security warning in Flow " + str(flow_id) + " parameter: " + param
+    if ( flow_results_db[flow_id][param][-1] >  threshold_up ) or ( flow_results_db[flow_id][param][-1] < threshold_down ) :
+        print "Security warning in Flow " + str(flow_id) + " parameter: " + param
         
         return False
+    
+    values_db[flow_id].pop(0)
+    values_db[flow_id].append(flow_results_db[flow_id][param][-1])
     return True
 
 def process_flows():
@@ -107,9 +130,11 @@ if __name__ == '__main__':
     #INIT
     for i in range(0, N_FLOWS):
         flow_results_db[i] = { 'timestamp' : [], 'packets_per_second' : [], 'bytes_per_second' : [], 'bad_flow' : False }
-    
+        values_db[i] = []
+
     #START FLOW LOOP
-    while(True):
+    while(T<=T_MAX):
+        print "HI. Current time: " + str(T)
         read_flows(T)
         process_flows()
         #time.sleep(TIME_WINDOW)
